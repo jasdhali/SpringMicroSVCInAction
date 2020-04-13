0f95a2a5450fdb4a6b01a2f84065f1621456f582
Jaspal Dhaliwal <83673@global.ul.com>
2020-04-12 21:53:11 -0500

    licensing service files

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/EagleEyeLicensingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.licenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleEyeLicensingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleEyeLicensingServiceApplication.class, args);
+	}
+
+}
